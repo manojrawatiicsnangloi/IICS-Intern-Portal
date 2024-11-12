f1dfa550e517ce44bff0bc1f961763994f5a84c8
unknown <manojrawat87634@gmail.com>
2024-11-12 10:32:43 +0530

    dev-version-2.0

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,6 @@ import connectionToDatabase from "./db/connection.js";
 
 const app = express();
 
-
 app.listen(3000,()=>{
     console.log("server Running on port 3000");
     connectionToDatabase();
--- backend/src/model/user/user.model.js
+++ backend/src/model/user/user.model.js
@@ -5,6 +5,8 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
+    unique: true,
+    match: [/^\S+@\S+\.\S+$/, "Please use a valid email address."],
   },
   password: {
     type: String,
